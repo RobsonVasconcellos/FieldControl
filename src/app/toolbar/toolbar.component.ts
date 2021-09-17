6765b1a0d2d142db3395207a617660e3a71a36fd
Robson Vasconcellos <rrvasconcellos@outlook.com>
2021-09-17 08:10:07 -0300

    Initial Commit

---
+++ src/app/toolbar/toolbar.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-toolbar',
+  templateUrl: './toolbar.component.html',
+  styleUrls: ['./toolbar.component.css']
+})
+export class ToolbarComponent implements OnInit {
+  private query = '';
+  searchForm = new FormControl('');
+
+  @Output() search = new EventEmitter<string>();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  startSearch() {
+    this.query = this.searchForm.value;
+    this.search.emit(this.query);
+  }
+}
